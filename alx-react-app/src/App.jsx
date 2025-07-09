ad6b23992c8bd4bc5b36a0d9cc6aa66748ad9b65
Max <murariax@gmail.com>
2025-07-09 10:07:15 +0300

    user profiles

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';
+import UserProfile from './components/UserProfile';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -36,6 +37,8 @@ function App() {
           <Header />
           <MainContent />
           <Footer />
+          <UserProfile 
+        name="Alice" age="25" bio="Loves hiking and photography"/>
     </>
   )
 
