34ed45a88fdd95b3a2b9d5285f7f5cf9cbd02b7b
Max <murariax@gmail.com>
2025-07-19 13:21:43 +0300

    user details

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -8,6 +8,8 @@ import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';
 import UserProfile from './components/UserProfile';
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 
 
 function App() {
@@ -52,6 +54,15 @@ function App() {
       <Counter />
     </div>
   );
+
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
+  
 }
 
 export default App;
---
+++ alx-react-app-new/src/components/ProfilePage.jsx
@@ -0,0 +1,7 @@
+const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
